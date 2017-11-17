9b21113a1b2f3660d13b3a9e8655d48194cca178
nukeop <alsw@protonmail.com>
2017-11-17 11:11:49 +0100

    Basic webpack config

---
+++ webpack.config.dev.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'dist');
+const APP_DIR = path.resolve(__dirname, 'game');
+const RESOURCES_DIR = path.resolve(__dirname, 'resources');
+
+module.exports = {
+  entry: path.resolve(APP_DIR, 'index.js'),
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+	test: /\.jsx?/,
+	loader: 'babel-loader',
+	include: APP_DIR
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'file-loader',
+        include: RESOURCES_DIR
+      },
+      {
+        test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader : 'file-loader',
+	include: RESOURCES_DIR
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.NamedModulesPlugin()
+  ]
+}
